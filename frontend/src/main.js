e2fd20ec11227d25b1c3e075cff2583247625017
tem <artish357@gmail.com>
2021-07-12 12:21:42 -0400

    Remove element-plus

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import installElementPlus from './plugins/element'
 
 const app = createApp(App).use(router)
-installElementPlus(app)
 app.use(router).mount('#app')
